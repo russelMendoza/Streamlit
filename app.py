57aa31fc8f7fa1d70661a024a057eda0254c86f6
russelMendoza <158264502+russelMendoza@users.noreply.github.com>
2024-05-18 14:26:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,25 +37,23 @@ def predict(image):
 # Streamlit app
 st.title("Eye Disease Detection System")
 
-# Display the subheader
-st.subheader("Team 12 Final Exam: Model Deployment in the Cloud")
 
 # Left corner for "Team 12 Final Exam: Model Deployment in the Cloud"
-st.sidebar.write("## Team 12 Final Exam: Model Deployment in the Cloud")
+st.sidebar.write("## [Team 12 Final Exam", "Model Deployment in the Cloud"])
 
 # Left corner for "Choose App Section"
 app_mode = st.sidebar.radio("", ["App Details", "Eye Disease Detection"])
 
 # App Details tab
-if app_mode == "App Details":
-    st.write("## About")
-    st.write("This app uses a deep learning model to detect common eye diseases from retinal images.")
-    st.write("Upload an image to predict the likelihood of various eye diseases.")
+if app_mode == "Model Details":
+    st.write("## About this Model")
+    st.write("This app uses a CNN deep learning model to detect common eye diseases from retinal images. It shows the probability level of an eye disease based on the four conditions currently present on the model:")
+    st.write(["Cataract", "Diabetic Retinopathy", "Glaucoma", "Normal")
 
 # Eye Disease Detection tab
 elif app_mode == "Eye Disease Detection":
     # File uploader
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Upload an image to predict the likelihood of various eye diseases.", type=["jpg", "jpeg", "png"])
 
     # Process uploaded image and make predictions
     if uploaded_file is not None:
