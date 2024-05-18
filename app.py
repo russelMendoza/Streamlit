d4ca975a05fcd50ca535b2f3361be2dba76bec4f
russelMendoza <158264502+russelMendoza@users.noreply.github.com>
2024-05-18 12:01:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 import cv2
+import io
 
 
 @st.cache_resource
@@ -27,14 +28,17 @@ st.write("""
 
 file = st.file_uploader("Choose retina image to identify the condition", type=["jpg", "png", "jpeg"])
 
+
 def import_and_predict(image_data, model):
     try:
         size = (64, 64)  # Ensure the input size matches the model's expected input size
-        image = Image.open(image_data)
+        # Convert image_data to BytesIO object
+        image_stream = io.BytesIO(image_data.read())
+        image = Image.open(image_stream)
         image = image.convert("RGB")  # Convert to RGB mode if necessary
         image = ImageOps.fit(image, size, Image.ANTIALIAS)
         img = np.asarray(image)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR for OpenCV
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Ensure image is in RGB format
         img_reshape = img[np.newaxis, ...]  # Reshape image to match model's input shape
         prediction = model.predict(img_reshape)
         return prediction
