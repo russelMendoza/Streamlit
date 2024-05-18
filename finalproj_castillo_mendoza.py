bddc58019ba02b817816cd13b7ef5432b470dce4
russelMendoza <158264502+russelMendoza@users.noreply.github.com>
2024-05-18 10:22:26 +0800

    Finalproj_castillo_mendoza.py

--- finalproj_castillo_mendoza.py
+++ finalproj_castillo_mendoza.py
@@ -10,8 +10,6 @@ Original file is located at
 from google.colab import drive
 drive.mount('/content/drive')
 
-pip install streamlit
-
 import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
